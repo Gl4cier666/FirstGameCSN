dd1852038f5d92dff990e444e6ea5f30faf14605
Gl4ier <Damiensandoval15@gmail.com>
2015-05-19 12:05:45 -0700

    html first and most likely final commit

---
+++ my-FG-game-html/src/com/FG/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.FG.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.FG.game.FirstGameMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new FirstGameMain();
+        }
+}
