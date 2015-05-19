d6c1c1d5c3f9c5f93bf004bdae7f6a1f288ee1f4
Gl4ier <Damiensandoval15@gmail.com>
2015-05-19 12:03:54 -0700

    Desktop First Commit

---
+++ my-FG-game-desktop/src/com/FG/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.FG.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.FG.game.FirstGameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FirstGameMain(), config);
+	}
+}
