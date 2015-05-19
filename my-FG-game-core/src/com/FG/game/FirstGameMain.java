6e66f9eacfd054aaaa31439c687603341de0b11f
ZGAMER666 <zwmail547749@gmail.com>
2015-05-19 12:36:55 -0700

    Testing access on github

--- my-FG-game-core/src/com/FG/game/FirstGameMain.java
+++ my-FG-game-core/src/com/FG/game/FirstGameMain.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+//Zack was here
 public class FirstGameMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
